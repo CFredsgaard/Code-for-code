62fa14e2d10de75b90fd5bdf73364fb0204fec9d
CFredsgaard <63876775+CFredsgaard@users.noreply.github.com>
2020-06-18 04:23:38 +0200

    tested some js

--- js/sololearn.js
+++ js/sololearn.js
@@ -1,10 +1,13 @@
 //Variables: 
-
 var x = 100; 
 
-var var_monkey = 'Hi friends'
+var var_monkey = 'Hi friends!';
+
+var text = 'My name is \'Cecilie\''
+
+//Statements:
+document.write(var_monkey);
 
 document.write(x);
 
-document.write(var_monkey);
-
+document.write(text);
